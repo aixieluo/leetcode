6901624cd9184da490a93b43c213daed18bc4962
39 <admin@aixieluo.com>
2021-05-09 11:59:30 +0800

    0509

---
+++ go/559. N 叉树的最大深度.go
@@ -0,0 +1,64 @@
+// 给定一个 N 叉树，找到其最大深度。
+//
+//最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+//
+//N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（请参见示例）。
+//
+// 
+//
+//示例 1：
+//
+//
+//
+//输入：root = [1,null,3,2,4,null,5,6]
+//输出：3
+//示例 2：
+//
+//
+//
+//输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+//输出：5
+// 
+//
+//提示：
+//
+//树的深度不会超过 1000 。
+//树的节点数目位于 [0, 104] 之间。
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree
+
+package main
+
+func main()  {
+
+}
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func maxDepth(root *Node) int {
+	if root == nil {
+		return 0
+	}
+	nodes := []*Node{root}
+	deeps := []int{1}
+	max := 0
+	for i:=0; i< len(nodes); i++ {
+		n := nodes[i]
+		d := deeps[i]
+		if d > max {
+			max = d
+		}
+		for _,v :=range n.Children {
+			nodes = append(nodes, v)
+			deeps = append(deeps, d +1)
+		}
+	}
+	return max
+}
