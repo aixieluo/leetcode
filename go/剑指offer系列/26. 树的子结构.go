e06f41bf22b9917c084cec44c52a411a42721ef7
xuxiaoyang <admin@aixieluo.com>
2021-05-19 19:25:52 +0800

    0519

--- go/剑指offer系列/26. 树的子结构.go
+++ go/剑指offer系列/26. 树的子结构.go
@@ -1,7 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(isSubStructure(createTree([]int{1, 0, 1, -4, -3}), createTree([]int{1, -4})), false)
+	fmt.Println(isSubStructure(createTree([]int{10, 12, 6, 8, 3, 11}), createTree([]int{10, 12, 6, 8})), true)
+	fmt.Println(isSubStructure(createTree([]int{4, 2, 3, 4, 5, 6, 7, 8, 9}), createTree([]int{4, 8, 9})), true)
 }
 
 type TreeNode struct {
@@ -10,29 +14,34 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func createTree(nums []int) *TreeNode  {
+func createTree(nums []int) *TreeNode {
 	nodes := make([]*TreeNode, 0, 10)
-	for _,v:=range nums {
-		nodes = append(nodes, &TreeNode{Val:v})
+	for _, v := range nums {
+		nodes = append(nodes, &TreeNode{Val: v})
 	}
-	for i:=1; i< len(nodes); i++
+	for i := 1; i < len(nodes); i++ {
+		if i%2 == 1 {
+			nodes[i/2].Left = nodes[i]
+		} else {
+			nodes[(i-1)/2].Right = nodes[i]
+		}
+	}
+	return nodes[0]
 }
 
 func isSubStructure(A *TreeNode, B *TreeNode) bool {
 	if B == nil || A == nil {
 		return false
 	}
-	if A.Val == B.Val {
-		if B.Left == nil && B.Right == nil {
-			return A.Left == nil && A.Right == nil
-		}
-		if B.Left == nil {
-			return isSubStructure(A.Right, B.Right) && A.Left == nil
-		} else if B.Right == nil {
-			return isSubStructure(A.Left, B.Left) && A.Right == nil
-		} else {
-			return isSubStructure(A.Left, B.Left) && isSubStructure(A.Right, B.Right)
-		}
+	return isSub(A, B) || isSubStructure(A.Left, B) || isSubStructure(A.Right, B)
+}
+
+func isSub(p, c *TreeNode) bool {
+	if c == nil {
+		return true
 	}
-	return isSubStructure(A.Left, B) || isSubStructure(A.Right, B)
+	if p == nil {
+		return false
+	}
+	return p.Val == c.Val && isSub(p.Left, c.Left) && isSub(p.Right, c.Right)
 }
